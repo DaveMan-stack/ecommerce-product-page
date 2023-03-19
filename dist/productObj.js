89a6caff6c344c2b455d026f55653401a7a17a8a
David Conteh <75188279+DaveMan-stack@users.noreply.github.com>
2023-03-19 18:50:18 +0000

    Add files via upload

---
+++ dist/productObj.js
@@ -0,0 +1,10 @@
+products = {
+    sneakersOne: {
+        thumbnails: 
+        ['../images/image-product-1-thumbnail.jpg', 
+        '../images/image-product-2-thumbnail.jpg', 
+        '../images/image-product-3-thumbnail.jpg', 
+        '../images/image-product-4-thumbnail.jpg'],
+        
+    }
+}
